c8df7b42f819d03b3d9e2351faa1ce2842329fae
Wertuyrw <zhikharko04@mail.ru>
2024-09-08 11:34:26 +0300

    feat: add basic logic fro DBContext

--- Infrastructure/LibraryDBContext.cs
+++ Infrastructure/LibraryDBContext.cs
@@ -1,12 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Domain.Entity;
+using Microsoft.EntityFrameworkCore;
 
 namespace Infrastructure
 {
-    internal class LibraryDBContext
+    public class LibraryDBContext : DbContext
     {
+        public LibraryDBContext(DbContextOptions<LibraryDBContext> options) : base(options) { }
+
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Account> Accounts { get; set; }
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<BookAuthor> BookAuthors { get; set; }
     }
 }
