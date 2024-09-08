8e45c383b5319eda313cb770bce209bf36e25376
Wertuyrw <zhikharko04@mail.ru>
2024-09-08 14:10:26 +0300

    feat: add middleware files

---
+++ LibraryAPI/Middlewares/ExceptionMiddleware.cs
@@ -0,0 +1,76 @@
+﻿using Application.DTO;
+using Domain.Exceptions;
+using System.Net;
+using System.Security.Authentication;
+
+namespace LibraryAPI.Middlewares
+{
+    public class ExceptionMiddleware
+    {
+        private readonly ILogger<ExceptionMiddleware> _logger;
+
+        private readonly RequestDelegate _requestDelegate;
+
+        public ExceptionMiddleware(RequestDelegate requestDelegate, ILogger<ExceptionMiddleware> logger)
+        {
+            _requestDelegate = requestDelegate;
+            _logger = logger;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                await _requestDelegate(context);
+            }
+            catch (ExistsException e)
+            {
+                await HandleExceptionAsync(context,
+                    e.Message,
+                    HttpStatusCode.BadRequest,
+                    e.Message);
+            }
+            catch (NotFoundException e)
+            {
+                await HandleExceptionAsync(context,
+                    e.Message,
+                    HttpStatusCode.NotFound,
+                    e.Message);
+            }
+            catch (AuthenticationException e)
+            {
+                await HandleExceptionAsync(context,
+                    e.Message,
+                    HttpStatusCode.Unauthorized,
+                    e.Message);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+
+                _logger.LogTrace(e.InnerException.ToString());
+
+                await HandleExceptionAsync(context,
+                    e.Message,
+                    HttpStatusCode.InternalServerError,
+                    e.Message);
+            }
+        }
+
+        private async Task HandleExceptionAsync(HttpContext context, string exMsg, HttpStatusCode httpStatusCode,
+            string message)
+        {
+            var response = context.Response;
+            response.ContentType = "application/json";
+            response.StatusCode = (int)httpStatusCode;
+
+            var errorDto = new ErrorDTO
+            {
+                StatusCode = (int)httpStatusCode,
+                Message = message
+            };
+
+            await response.WriteAsJsonAsync(errorDto);
+        }
+    }
+}
